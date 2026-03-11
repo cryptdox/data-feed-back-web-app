5d1565ac67349e528a189fcdd9384907b9376672
abir-hosen-ashik <abir71.hosen@gmail.com>
2026-03-11 13:18:43 +0600

    sidebar

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -17,34 +17,35 @@ export function MainLayout({ children, currentPage, onNavigate }: MainLayoutProp
     return <>{children}</>;
   }
 
-  return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-950 flex flex-col">
-      <Navbar onToggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
-      <div className="flex flex-1">
-        {/* Sidebar for large screens */}
-        <div className="hidden md:block">
+  return (<div className="min-h-screen bg-gray-50 dark:bg-gray-950 flex flex-col">
+    <Navbar onToggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
+    <div className="flex flex-1">
+      {/* Sidebar for large screens */}
+      <div className="hidden md:block">
+        <div className="fixed top-16 left-0 w-64 h-[calc(100vh-4rem)] bg-white dark:bg-gray-900 border-r border-gray-200 dark:border-gray-700 p-4 overflow-y-auto overflow-x-hidden scrollbar-none">
           <Sidebar currentPage={currentPage} onNavigate={onNavigate} />
         </div>
-
-        {/* Sidebar overlay for small screens */}
-        {sidebarOpen && (
-  <div className="fixed inset-0 z-40 md:hidden">
-    {/* dark overlay */}
-    <div
-      className="fixed inset-0 bg-black opacity-50"
-      onClick={() => setSidebarOpen(false)}
-    />
-
-    {/* sidebar container */}
-    <div className="relative w-64 bg-white dark:bg-gray-900 border-r border-gray-200 dark:border-gray-700 h-full p-4 pt-16 z-50 overflow-y-auto overflow-x-hidden scrollbar-none">
-  <Sidebar currentPage={currentPage} onNavigate={onNavigate} />
-</div>
-  </div>
-)}
-
-        {/* Main content */}
-        <main className="flex-1 p-4 md:p-8">{children}</main>
       </div>
+  
+      {/* Sidebar overlay for small screens */}
+      {sidebarOpen && (
+        <div className="fixed inset-0 z-40 md:hidden">
+          {/* dark overlay */}
+          <div
+            className="fixed inset-0 bg-black opacity-50"
+            onClick={() => setSidebarOpen(false)}
+          />
+  
+          {/* sidebar container */}
+          <div className="relative w-64 bg-white dark:bg-gray-900 border-r border-gray-200 dark:border-gray-700 h-full p-4 pt-16 z-50 overflow-y-auto overflow-x-hidden scrollbar-none">
+            <Sidebar currentPage={currentPage} onNavigate={onNavigate} />
+          </div>
+        </div>
+      )}
+  
+      {/* Main content */}
+      <main className="flex-1 p-4 md:p-8 md:ml-64">{children}</main>
     </div>
+  </div>
   );
 }
